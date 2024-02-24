8eaf7ef2d47efd78f0207e4668c60ec1164f2641
Chayan Dutta <dipakchatterjee957@gmail.com>
2024-02-24 21:17:45 +0530

    node_modules be gone!

--- router/user.router.js
+++ router/user.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const userRouter = express.Router();
 const userController = require('../controller/user.controller');
 
-userRouter.get(`/getUserList`, userController.getUserList)
+userRouter.get(`/getUserList`, userController.getUserList);
 
 module.exports = userRouter
